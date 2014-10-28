6e218d21ddbbeafe6315c6268c126ae71259f0a4
lunaj388 <lunaj388@gmail.com>
2014-10-27 17:14:52 -0700

    mac

---
+++ lab9.1/lab9.1/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  lab9.1
+//
+//  Created by Jose Luna on 10/27/14.
+//  Copyright (c) 2014 Jose Luna. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int length;
+    int width;
+    int area;
+    
+    int *lengthPtr;
+    int widthPtr;
+    
+    cout<<"Please input the length of the rectangle"<<endl;
+    cin>>length;
+    cout<<"Please intput the widthnof the rectangle"<<endl;
+    cin>>width;
+    
+    lengthPtr = &length;
+    widthPtr = &width;
+    
+    area = length
+    return 0;
+}
